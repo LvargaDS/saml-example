9efda62854e5e621f14f06b01fa0c08af155c2c6
Ľubomír Varga <151617208+LvargaDS@users.noreply.github.com>
2023-12-06 10:48:09 +0100

    Adapt project to java 8
    Use maximum pcap4j dependency, which is using 3.X opensaml dependency.
    Version 4.x is compiled for java 11. See
    https://shibboleth.atlassian.net/wiki/spaces/DEV/pages/1161266638/Product+Platforms
    Only change is the need to implement transferTo method, as it is java 9 thing.

--- src/main/java/ca/redtoad/Home.java
+++ src/main/java/ca/redtoad/Home.java
@@ -4,12 +4,26 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 public class Home extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html");
         response.setStatus(HttpServletResponse.SC_OK);
-        getClass().getResourceAsStream("/home.html").transferTo(response.getOutputStream());
+        transferTo(getClass().getResourceAsStream("/home.html"), response.getOutputStream());
+    }
+
+    public long transferTo(InputStream is, OutputStream out) throws IOException {
+        final int DEFAULT_BUFFER_SIZE = 8192;
+        long transferred = 0;
+        byte[] buffer = new byte[DEFAULT_BUFFER_SIZE];
+        int read;
+        while ((read = is.read(buffer, 0, DEFAULT_BUFFER_SIZE)) >= 0) {
+            out.write(buffer, 0, read);
+            transferred += read;
+        }
+        return transferred;
     }
 }
